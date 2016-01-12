c2074ef8412c6cbfae3d4fa0a51ee6fe3c13d894
Reza <rezagama@yahoo.co.id>
2016-01-13 01:22:06 +0700

    add google maps feature

--- app/src/main/java/com/fastforward/checkin/MainActivity.java
+++ app/src/main/java/com/fastforward/checkin/MainActivity.java
@@ -1,8 +1,11 @@
 package com.fastforward.checkin;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.ConnectionCallbacks,
+                    GoogleApiClient.OnConnectionFailedListener{
+
+    private double lat, lng;
+    private GoogleMap mMap;
+    private Location mLocation;
+    private GoogleApiClient googleApiClient;
+    private static final int PLACE_PICKER_REQUEST = 1;
+    private TextView place, address, phone, currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +53,28 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mapInit();
+
+        currentPos = (TextView) findViewById(R.id.currentPos);
+        place      = (TextView) findViewById(R.id.place);
+        address    = (TextView) findViewById(R.id.address);
+        phone      = (TextView) findViewById(R.id.phone);
+
+        googleApiClient = new GoogleApiClient
+                .Builder( this )
+                .enableAutoManage( this, 0, this )
+                .addApi( Places.GEO_DATA_API )
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi (LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                launchPlacePicker();
             }
         });
 
@@ -49,6 +95,55 @@ public class MainActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if(mLocation == null){
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if(mMap != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), 13));
+                mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()))
+                                .title("You're Here")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                );
+                lat = mLocation.getLatitude();
+                lng = mLocation.getLongitude();
+
+                currentPos.setText("Current Position : " + String.valueOf(lat) + ' ' + String.valueOf(lng));
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapInit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(googleApiClient.isConnected()){
+            googleApiClient.disconnect();
         }
     }
 
@@ -97,5 +192,48 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void mapInit(){
+        if(mMap == null){
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMap();
+
+            if(mMap != null){
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap(){
+        mMap.setMyLocationEnabled(true);
+    }
+
+    private void launchPlacePicker(){
+        if(googleApiClient == null || !googleApiClient.isConnected()){
+            return;
+        }
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try{
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        }catch (GooglePlayServicesRepairableException e){
+            e.printStackTrace();
+        }catch (GooglePlayServicesNotAvailableException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK){
+            displayPickerResult(PlacePicker.getPlace(data, this));
+        }
+    }
+
+    private void displayPickerResult(Place location){
+        place.setText(location.getName());
+        address.setText(location.getAddress());
+        phone.setText(location.getPhoneNumber());
     }
 }
